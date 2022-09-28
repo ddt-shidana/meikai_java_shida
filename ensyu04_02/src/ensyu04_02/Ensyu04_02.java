27126caace305302842e5403e31fa4bc60c37101
ddt-shidana <ddt.kzn.shida@gmail.com>
2022-09-28 19:58:33 +0900

    ensyu04_02 指摘反映 （変数名を変更） 20220928

--- ensyu04_02/src/ensyu04_02/Ensyu04_02.java
+++ ensyu04_02/src/ensyu04_02/Ensyu04_02.java
@@ -25,11 +25,11 @@ public class Ensyu04_02 {
 		//randに乱数を初期設定
 		Random rand = new Random();
 
-		//整数型変数noに、（0～89）＋10の乱数randを初期設定
-		int no = rand.nextInt(90) + 10;
+		//整数型変数random_intに、（0～89）＋10の乱数randを初期設定
+		int random_int = rand.nextInt(90) + 10;
 
-		//ゲームプレイする人が数当てで入力する値を格納するための、整数型変数xを用意
-		int xInt;
+		//ゲームプレイする人が数当てで入力する値を格納するための、整数型変数answerを用意
+		int answer;
 
 		//ゲームスタートの文言を画面表示させる
 		System.out.println("数当てゲーム開始!!\n10～99の値を入力してください。");
@@ -39,24 +39,23 @@ public class Ensyu04_02 {
 			System.out.print("いくつかな？：");
 
 			//あらかじめ用意していたxにゲームプレイ者が入力した値を代入
-			xInt = stdIn.nextInt();
+			answer = stdIn.nextInt();
 
-			//入力値(x)がnoより小さい時
-			if (xInt > no) {
+			//入力値(x)がrandom_intより小さい時
+			if (answer > random_int) {
 				//「もっと小さい数だよ」と画面表示させる
 				System.out.println("もっと小さい数だよ");
-			//入力値(x)がnoより大きい時
-			} else if (xInt < no) {
+			//入力値(x)がrandom_intより大きい時
+			} else if (answer < random_int) {
 				//「もっと大きい数だよ」と画面表示させる
 				System.out.println("もっと大きい数だよ");
 			}
 
-		//doメソッドここまで
 		}
-		//入力値xがゲームスタート時に生成した乱数(no)と一致しない間、doメソッドを繰り返す
-		while (xInt != no);
+		//入力値xがゲームスタート時に生成した乱数(random_int)と一致しない間、doメソッドを繰り返す
+		while (answer != random_int);
 
-		//入力値xがゲームスタート時に生成した乱数(no)と一致したら「正解です。」と画面表示させる
+		//入力値xがゲームスタート時に生成した乱数(random_int)と一致したら「正解です。」と画面表示させる
 		System.out.println("正解です。");
 
 		//newされているので、scannerのインスタンス(stdIn)をクローズ
