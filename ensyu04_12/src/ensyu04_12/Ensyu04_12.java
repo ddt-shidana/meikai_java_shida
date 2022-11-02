7977e1e042e81a924bcd90ff2376608f52e0a5db
ddt-shidana <ddt.kzn.shida@gmail.com>
2022-11-02 21:00:10 +0900

    ensyu04_12 命名規則に則って修正 20221102 100%

--- ensyu04_12/src/ensyu04_12/Ensyu04_12.java
+++ ensyu04_12/src/ensyu04_12/Ensyu04_12.java
@@ -19,29 +19,30 @@ public class Ensyu04_12 {
 	public static void main(String[] args) {
 		
 		//Scannerクラスのインスタンス生成
-		Scanner stdIn = new Scanner(System.in);
+		Scanner standardInput = new Scanner(System.in);
 		//カウントアップを宣言
 		System.out.println("カウントアップします。");
 		
 		//正の整数値
-		int zMax = 0;
-		//正の整数値を聞く塊
+		int positiveInteger = 0;
+		
+		//正の整数値を聞くかたまり
 		do {
 			//正の整数値の入力ナビ
 			System.out.print("正の整数値：");
 			//キーボードからの入力値を受け取る
-			zMax = stdIn.nextInt();
+			positiveInteger = standardInput.nextInt();
 		//0以下だったら聞き直す
-		} while (zMax <= 0);
+		} while (positiveInteger <= 0);
 		
-		//0からzMaxまで
-		for (int i = 0; i <= zMax; i++) {
+		//0からpositiveIntegerまで
+		for (int countIndex = 0; countIndex <= positiveInteger; countIndex++) {
 			//1ずつ増やした値を画面表示
-			System.out.println(i);
+			System.out.println(countIndex);
 		}
 
 		//newされているので、scannerのインスタンス(stdIn)をクローズ
-		stdIn.close();
+		standardInput.close();
 
 	}
 }
